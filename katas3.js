a990f4e31f9c1f18835b99f12d72df1d18d604c9
Nathan Ribeiro <nathan.ribeiros7@gmail.com>
2022-08-26 17:51:34 -0300

    envio katas3

--- katas3.js
+++ katas3.js
@@ -1,74 +1,219 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
-function kata1() {
-    // implemente o código do kata 1 aqui
+function showResults(valor){
+   return console.log(valor)
 }
 
-function kata2() {
-    // implemente o código do kata 2 aqui
+
+function kata1(num) {  
+    let result = []
+    for(let i = 1; i<=num; i++) {
+        result.push(i)
+    }
+   return result
+}
+let sequencia = kata1(25)
+showResults(sequencia)
+
+function kata2(num) {
+    let result = []
+    for(let i = num; i>0; i--) {
+        result.push(i)
+    }
+   return result
+}
+let reverseSeq = kata2(25)
+showResults(reverseSeq)
+
+
+function kata3(num) {
+    let result = []
+    for(let i = -1; i>=num; i--) {
+        result.push(i)
+    }
+   return result
+}
+let negatievSeq = kata3(-25)
+showResults(negatievSeq)
+
+
+function kata4(num) {
+    let result = []
+    for (let i=num; i<0; i++){
+        result.push(i)
+    }
+    return result
 }
 
-function kata3() {
-    // implemente o código do kata 3 aqui
-}
+let reverseNegSeq = kata4(-25)
+showResults(reverseNegSeq)
 
-function kata4() {
-    // implemente o código do kata 4 aqui
+function kata5(num) {
+    let result = []
+    for (let i=num; i>=-num; i--){
+       if(i%2 !== 0 ) {
+        result.push(i)
+    }}
+    return result
 }
+let impars = kata5(25)
+showResults(impars)
 
-function kata5() {
-    // implemente o código do kata 5 aqui
-}
 
-function kata6() {
-    // implemente o código do kata 6 aqui
+function kata6(num) {
+    let result = []
+    for (let i=1; i<=num; i++){
+       if(i%3 === 0 ) {
+        result.push(i)
+    }}
+    return result
 }
+let divisiveispor3 = kata6(100)
+showResults(divisiveispor3)
 
-function kata7() {
-    // implemente o código do kata 7 aqui
+function kata7(num) {
+    
+    let result = []
+    for (let i=1; i<=num; i++){
+       if(i %7 === 0 ) {
+        result.push(i)
+    }}
+    return result
 }
+let divisiveispor7 = kata7(100)
+showResults(divisiveispor7)
 
-function kata8() {
-    // implemente o código do kata 8 aqui
-}
 
-function kata9() {
-    // implemente o código do kata 9 aqui
+function kata8(num) {
+    let result = []
+    for (let i = num; i>0; i--){
+        if(i%3 === 0 || i%7===0 ) {
+            result.push(i)
+        }
+    }
+    return result
 }
+let div3OuDiv9Reverse = kata8(100)
+showResults(div3OuDiv9Reverse)
 
-function kata10() {
-    // implemente o código do kata 10 aqui
-}
 
-function kata11() {
-    // implemente o código do kata 11 aqui
+function kata9(num) {
+    let result = []
+    for (let i = 1; i<=num; i++){
+        if(i%2 !== 0 && i%5===0 ) {
+            result.push(i)
+        }
+    }
+    return result
 }
+let imparsDivPor5 = kata9(100)
+showResults(imparsDivPor5) 
 
-function kata12() {
-    // implemente o código do kata 12 aqui
-}
 
-function kata13() {
-    // implemente o código do kata 13 aqui
-}
 
-function kata14() {
-    // implemente o código do kata 14 aqui
+function kata10(arr) { 
+    let valoresArr = ""
+    for (let i = 0; i<arr.length; i++) {
+        valoresArr += `${arr[i]}, `
+    }
+    return valoresArr
 }
+let exibindoSample = kata10 (sampleArray)
+showResults(exibindoSample) 
 
-function kata15() {
-    // implemente o código do kata 15 aqui
-}
 
-function kata16() {
-    // implemente o código do kata 16 aqui
+function kata11(arr) {
+      let valoresArr = ""
+    for (let i = 0; i<arr.length; i++) {
+        if(arr[i] %2 ===0)
+        valoresArr += `${arr[i]}, `
+    }
+    return valoresArr
 }
+let exibindoParSample = kata11 (sampleArray)
+showResults(exibindoParSample) 
 
-function kata17() {
-    // implemente o código do kata 17 aqui
-}
 
-function kata18() {
-    // implemente o código do kata 18 aqui
+function kata12(arr) {
+    let valoresArr = ""
+    for (let i = 0; i<arr.length; i++) {
+        if(arr[i] %2 !==0)
+        valoresArr += `${arr[i]}, `
+    }
+    return valoresArr
 }
+let exibindoImparSample = kata12 (sampleArray)
+showResults(exibindoImparSample)
+
+
+function kata13(arr) {
+    let valoresArr = ""
+    for(let i = 0; i<arr.length; i++) {
+        if(arr[i] % 8 === 0){
+            valoresArr += `${arr[i]}, ` 
+        }
+    }
+    return valoresArr
+}
+let exibeDiv8Sample = kata13(sampleArray)
+showResults(exibeDiv8Sample)
+
+
+function kata14(arr) {
+    let valoresArr = ""
+    for(let i = 0; i<arr.length; i++) {
+            valoresArr += `${arr[i]*arr[i]}, ` 
+    }
+    return valoresArr
+}
+let exibeQuadrSample = kata14(sampleArray)
+showResults(exibeQuadrSample)
+ 
+
+function kata15(num) {
+    let somando = 0
+    for(let i = 0; i<=num; i++){
+        somando += i
+    }
+    return somando 
+}    
+let somaAllNum = kata15(20) 
+showResults(somaAllNum)
+
+
+function kata16(arr) {
+    let somando = 0
+    for(let i = 0; i<arr.length; i++){
+        somando += arr[i]
+    }
+    return somando 
+}
+let somaAllArr = kata16(sampleArray) 
+showResults(somaAllArr)
+
+
+function kata17(arr) {
+    let maior = 0
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]> maior){
+            maior = arr[i]
+        }
+    }
+    return maior
+}
+let MaiorFromArr = kata17(sampleArray)
+showResults(MaiorFromArr)
+
+
+function kata18(arr) {
+    let menor = arr[0]
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]< menor){
+            menor = arr[i]
+        }
+    }
+    return menor
+}
+let MenorFromArr = kata18(sampleArray)
+showResults(MenorFromArr)
 
